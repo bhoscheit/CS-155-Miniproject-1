bb92eba3fd7eb477649c1065ba697ab81824fad6
dylanbannon <dylanbannon5@gmail.com>
2018-02-04 18:21:39 -0800

    data and laebl input are now functional

--- dylan_input_script.py
+++ dylan_input_script.py
@@ -1,1 +1,12 @@
+# Imports data and creates label vector.
+
 import numpy as np
+import csv
+
+#import pdb
+
+training_data = np.loadtxt("training_data.txt", skiprows=1)
+with open('training_data.txt') as f:
+    reader = csv.reader(f, delimiter=" ")
+    labels = next(reader)
+#pdb.set_trace()
