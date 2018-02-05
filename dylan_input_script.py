c57ebeccac78b33aac8d87a160e7a3332860d0df
dylanbannon <dylanbannon5@gmail.com>
2018-02-04 18:41:54 -0800

    now dividing data into test and training sets

--- dylan_input_script.py
+++ dylan_input_script.py
@@ -3,10 +3,20 @@
 import numpy as np
 import csv
 
-#import pdb
+import pdb
 
+# Import data and labels
 training_data = np.loadtxt("training_data.txt", skiprows=1)
 with open('training_data.txt') as f:
     reader = csv.reader(f, delimiter=" ")
     labels = next(reader)
-#pdb.set_trace()
+
+# Split data into test an training subsets
+np.random.shuffle(training_data)
+number_of_data_points = training_data.shape[0]
+eighty_percent_cutoff = int(number_of_data_points*0.8)
+test_data = training_data[ eighty_percent_cutoff:number_of_data_points, : ]
+training_data = training_data[ 0:eighty_percent_cutoff, : ]
+
+# Pause the program and inspect the variables
+pdb.set_trace()
