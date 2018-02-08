f35deb9d741854453a75b4dcbd3e85a2c6035c2e
dylanbannon <dylanbannon5@gmail.com>
2018-02-08 01:23:59 -0800

    updated testing

--- dylan_logistic_reg_optimal.py
+++ dylan_logistic_reg_optimal.py
@@ -35,12 +35,23 @@ model.fit( training_x, training_y )
 test_result = model.predict( test_tf_idf )
 
 # Alternative where you train on 80% of the training data and test on the remaining 20%
-#np.random.shuffle(training_tf_idf)
-#training_x = training_tf_idf[0:16000,1:]
-#training_y = training_tf_idf[0:16000,0]
-#test_x = training_tf_idf[16000:,1:]
-#test_y = training_tf_idf[16000:,0]
-#model = LogisticRegression(penalty='l2', C = 0.975)
-#model.fit( training_x, training_y )
-#test_result = model.score( test_x, test_y )
-#print( str(test_result) )
+k_folds = 10
+
+kfold = StratifiedKFold(n_splits = k_folds, shuffle=True, random_state=4525)
+kfold.get_n_splits( training_tf_idf[:,1:], training_tf_idf[:,0])
+
+test_results = []
+for train_index, test_index in kfold.split( training_tf_idf[:,1:], training_tf_idf[:,0] ):
+    training_data = training_tf_idf[train_index]
+    test_data = training_tf_idf[test_index]
+    training_x = training_data[:,1:]
+    training_y = training_data[:,0]
+    test_x = test_data[:,1:]
+    test_y = test_data[:,0]
+    model = LogisticRegression(penalty='l2', C = 0.975)
+    model.fit( training_x, training_y )
+    test_result = model.score( test_x, test_y )
+    test_results.append(test_result)
+test_results = np.array(test_results)
+average = np.average(test_results)
+print( str(average) )
